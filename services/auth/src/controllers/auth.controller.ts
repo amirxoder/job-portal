278852fd91d717605d7f8fe1e5536df517a4a8ef
amirxoder <amirxoder@gmail.com>
2026-02-19 21:42:49 +0330

    add multer to parse files

--- services/auth/src/controllers/auth.controller.ts
+++ services/auth/src/controllers/auth.controller.ts
@@ -1,6 +1,37 @@
+import { sql } from "../utils/db.js";
+import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
+import bcrypt from "bcrypt";
 
 export const userRegister = TryCatch(async (req, res, next) => {
-  const { email } = req.body;
-  res.json(email);
+  const { name, email, password, phoneNumber, role, bio } = req.body;
+  if (!name || !email || !password || !phoneNumber || !role) {
+    throw new ErrorHandler(400, "All fields are required");
+  }
+
+  const existingUsers = await sql`SELECT * FROM users WHERE email = ${email}`;
+  if (existingUsers.length > 0) {
+    throw new ErrorHandler(409, "User with this email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  let registeredUser;
+
+  if (role === "recruiter") {
+    const [user] = await sql`
+      INSERT INTO users (nama, email, password, phone_number, role, bio) VALUES (${name}, ${email}, ${hashedPassword}, ${phoneNumber}, ${role}, ${bio}) RETURNING user_id, name, email, phone_number, role, created_at
+    `;
+
+    registeredUser = user;
+  } else if (role === "jobseeker") {
+    const file = req.file;
+    const [user] = await sql`
+      INSERT INTO users (nama, email, password, phone_number, role) VALUES (${name}, ${email}, ${hashedPassword}, ${phoneNumber}, ${role}) RETURNING user_id, name, email, phone_number, role, created_at
+    `;
+
+    registeredUser = user;
+  }
+
+  res.json(registeredUser);
 });
