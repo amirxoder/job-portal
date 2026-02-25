c1133e3c1ff9c0ac1f601a4bd141a555a9f26d4c
amirxoder <amirxoder@gmail.com>
2026-02-25 15:14:55 +0330

    impelement login service

--- services/auth/src/routes/auth.route.ts
+++ services/auth/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { userRegister } from "../controllers/auth.controller.js";
+import { loginUser, userRegister } from "../controllers/auth.controller.js";
 
 import uploadFile from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", uploadFile, userRegister);
+router.post("/login", loginUser);
 
 export default router;
